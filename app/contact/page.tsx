efa1aa525a60abc851b87d9cc61846bf540fd8ca
Prithvi Raj Rana <trialbuildsprr@gmail.com>
2026-05-31 13:19:45 +0530

    Fix contact page export

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <h1>Contact Page</h1>
+    </main>
+  );
+}
---
+++ app/trainers/page.tsx
@@ -0,0 +1,7 @@
+export default function TrainersPage() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <h1>Trainers Page</h1>
+    </main>
+  );
+}
